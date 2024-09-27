e4ba44fcef32ebee91b6725159ff09aecedf15b8
adil <rameezadil93@gmail.com>
2024-09-27 22:50:42 +0800

    update

---
+++ 1100/158B-Taxi.cpp
@@ -0,0 +1,15 @@
+// 158B - Taxi - Difficulty: 1100
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int n, s;
+  cin >> n;
+
+  for (int i = 0; i < n; i++) {
+  }
+
+  return 0;
+}
--- stats.sh
+++ stats.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+diff1100=$(ls 1100/ -1 | wc -l)
 diff1000=$(ls 1000/ -1 | wc -l)
 diff900=$(ls 900/ -1 | wc -l)
 diff800=$(ls 800/ -1 | wc -l)
 
-average=$(echo "scale=2; (1000 * $diff1000 + 900 * $diff900 + 800 * $diff800) / ($diff1000 + $diff900 + $diff800)" | bc)
+average=$(echo "scale=2; (1100 * $diff1100 + 1000 * $diff1000 + 900 * $diff900 + 800 * $diff800) / ($diff1100 + $diff1000 + $diff900 + $diff800)" | bc)
 
+echo "1100 difficulty problems solved: $diff1100"
 echo "1000 difficulty problems solved: $diff1000"
 echo "900 difficulty problems solved: $diff900"
 echo "800 difficulty problems solved: $diff800"
