51fce3ccc4ef71730ece64fd7b08e2497c74f1b1
adil <rameezadil93@gmail.com>
2024-10-01 21:46:42 +0800

    first commit

--- 1100/158B-Taxi.cpp
+++ 1100/158B-Taxi.cpp
@@ -5,11 +5,32 @@
 using namespace std;
 
 int main() {
-  int n, s;
+  int n, s, total(0);
   cin >> n;
 
-  for (int i = 0; i < n; i++) {
+  if (n == 1) {
+    cout << 1;
+    return 0;
   }
 
+  int count[5] = {0};
+  for (int i = 0; i < n; i++) {
+    cin >> s;
+    count[s]++; // increase either of the 4 indexes based on the number of
+                // kids in the group
+  }
+  total = count[4] + count[3] + count[2] / 2;
+  // the number of cars before individual kids are accounted for
+  count[1] -= count[3];    //
+  if (count[2] % 2 == 1) { // if there are odd number of two man groups
+    total += 1;
+    count[1] -= 2; // take 2 single to accomadate that 2 man
+  }
+  if (count[1] > 0) {
+    total += (count[1] + 3) / 4;
+    // add 3 to remaining individuals to make them divisible or roundable up
+  }
+
+  cout << total;
   return 0;
 }
